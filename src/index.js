b50c6bb9fff017ddff7604cbfe3ec42d1b769d22
akshkin <107682289+akshkin@users.noreply.github.com>
2022-10-26 12:57:32 +0200

    minor tweaks for deploymment

--- src/index.js
+++ src/index.js
@@ -19,10 +19,10 @@ app.use(userRouter)
 
 //serve static assets in production
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static("client/task-manager-client/build"))
+  app.use(express.static("client/task-manager-client/build/static"))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "task-manager-client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "task-manager-client", "build", "static", "index.html"))
   })
 }
 
